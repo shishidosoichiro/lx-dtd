fcccb02b94a52718615a5aeaa1840e6de6bcd1c7
Soichiro Shishido <shishidosoichiro@hotmail.com>
2015-11-30 00:57:44 +0000

    etc

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ var Lexer = require('lx');
 var state = Lexer.state;
 var back = Lexer.back;
 var shift = Lexer.shift;
-var token = Lexer.token;
-var push = Lexer.push;
+var token = function(name){
+  var token = Lexer.token(name);
+  return function(){
+    var t = token.apply(this, arguments);
+    Lexer.push.call(this, t);
+  }
+};
 var noop = Lexer.noop;
 var raise = Lexer.raise;
 
@@ -11,25 +16,59 @@ var tokenize = (function(){
   var doctype = Lexer()
   var node = Lexer()
   var element = Lexer()
-  element.arg = Lexer()
-  element.arg.category = Lexer()
-  element.arg.content = Lexer()
-  element.end = Lexer()
-  var attribute = Lexer()
-  var arg = Lexer()
+  element.content = Lexer()
+  element.content.suffix = Lexer()
+  element.content.delmmiter = Lexer()
+  var attlist = Lexer()
+  attlist.type = Lexer()
+  attlist.value = Lexer()
 
   doctype
-  .match(/<\!DOCTYPE\s+([\w\.\:\-]+)\s+\[/, shift, token('DOCTYPE'), push, state(node))
+  .match(/<\!DOCTYPE\s+([\w\.\:\-]+)\s+\[/, shift, token('DOCTYPE'), state(node))
+  .match(/\s+/, noop)
+  .other(raise('unexpected declaration.'))
+
+  node
+  .match(/<\!ELEMENT\s+([\w\.\:\-]+)\s+/, shift, token('ELEMENT'), state(element))
+  .match(/<\!ATTLIST\s+([\w\.\:\-]+)\s+/, shift, token('ATTLIST'), state(attlist))
+  .match(/\]>/, token('DOCTYPE.END'), back)
   .match(/\s+/, noop)
   .other(raise('unexpected statement.'))
 
+  element
+  .match(/^(EMPTY|ANY)\s*>/, shift, token('ELEMENT.CATEGORY'), back)  
+  .match(/^\(/, token('ELEMENT.CONTENT'), state(element.content))
+  .match(/^\s+/, noop)
+  .other(raise('invalid element difinition.'))
 
-  node
-//  .match(/<\!(ELEMENT)\s+/, shift, token('ELEMENT'), push, state(element))
-//  .match(/<\!(ATTRIBUTE)\s+/, shift, token('ATTRIBUTE'), push, state(attribute))
-  .match(/\]>/, token('DOCTYPE.END'), push, back)
-  .match(/\s+/, noop)
-  .other(raise('unexpected node declare.'))
+  element.content
+  .match(/^([\w\.\:\-]+)/, shift, token('ELEMENT.CONTENT.CHILD'), state(element.content.suffix))  
+  .match(/^(\#PCDATA)/, shift, token('ELEMENT.CONTENT.PCDATA'), state(element.content.delmmiter))
+  .match(/^\(/, token('ELEMENT.CONTENT'), state(element.content))
+  .match(/^\s+/, noop)
+  .other(raise('invalid element-content difinition.'))
+
+  element.content.suffix
+  .match(/^\+/, token('ELEMENT.CONTENT.MIN-ONE-OCCURENCE'), back, state(element.content.delmmiter))  
+  .match(/^\*/, token('ELEMENT.CONTENT.ZERO-OR-MORE-OCCURENCE'), back, state(element.content.delmmiter))  
+  .match(/^\?/, token('ELEMENT.CONTENT.ZERO-OR-ONE-OCCURENCE'), back, state(element.content.delmmiter))  
+  .match(/^(?=[\,\|\)]|\s*>)/, back, state(element.content.delmmiter))
+  .match(/^\s+/, noop)
+  .other(raise('invalid element-content suffix difinition.'))
+
+  element.content.delmmiter
+  .match(/^\,/, token('ELEMENT.CONTENT.SEQUENCE'), back)  
+  .match(/^\|/, token('ELEMENT.CONTENT.OR'), back)
+  .match(/^\)/, token('ELEMENT.CONTENT.CLOSE'), back, back, state(element.content.suffix))
+  .match(/^\s*>/, token('ELEMENT.END'), back, back, state(node))  
+  .match(/^\s+/, noop)
+  .other(raise('invalid element-content delmmiter difinition.'))
+
+  attlist
+  .match(/([\w\.\:\-]+)\s+/, shift, token('ATTR.NAME'), state(attlist.type))
+
+  attlist.type
+  .match(/([\w\.\:\-]+)\s+/, shift, token('ATTR.NAME'), state(attlist.value))
 
   return function(string){
     var context = {tokens: []};
--- test/index.js
+++ test/index.js
@@ -2,25 +2,186 @@ var chai = require('chai');
 var expect = chai.expect;
 
 var dtd = require('../index');
+var toString = function(e){
+	return e.toString();
+};
 
 describe('dtd', function(){
 
 	it('example', function(){
-		var text = '<!DOCTYPE note [\
+		var input = '<!DOCTYPE note [\
 <!ELEMENT note (to,from,heading,body)>\
 <!ELEMENT to (#PCDATA)>\
 <!ELEMENT from (#PCDATA)>\
 <!ELEMENT heading (#PCDATA)>\
 <!ELEMENT body (#PCDATA)>\
 ]>';
-//		var data = dtd.tokenize(text);
-	//	console.log(data);
+		var output = dtd.tokenize(input);
+		output = output.map(toString);
+//		console.log(output);
 	})
 
-	it('An Internal DTD Declaration', function(){
-		var text = '<!DOCTYPE note [\
+	describe('"declare" lexer', function(){
+		it('tokenizes DOCTYPE', function(){
+			var input = '<!DOCTYPE note [\
 ]>';
-		var data = dtd.tokenize(text);
-		console.log(data);
+			var output = dtd.tokenize(input);
+			output = output.map(toString);
+			expect(output[0]).to.equal('DOCTYPE(0:note)');
+		})
+		it('throws "unexpected declaration."', function(){
+			var input = 'null\
+<!DOCTYPE note [\
+]>';
+			var f = function(){return dtd.tokenize(input)};
+			expect(f).to.throw('unexpected declaration.')
+		})
+	})
+	describe('"node" lexer', function(){
+		it('tokenizes ELEMENT', function(){
+			var input = '<!DOCTYPE note [\
+<!ELEMENT note (to,from,heading,body)>\
+<!ELEMENT to (#PCDATA)>\
+<!ELEMENT from (#PCDATA)>\
+<!ELEMENT heading (#PCDATA)>\
+<!ELEMENT body (#PCDATA)>\
+]>';
+			var output = dtd.tokenize(input);
+			output = output.map(toString);
+			expect(output[1]).to.equal('ELEMENT(16:note)');
+		})
+	})
+	describe('"element" lexer', function(){
+		it('tokenizes ELEMENT.CATEGORY', function(){
+			var input = '<!DOCTYPE note [\
+<!ELEMENT note ANY>\
+<!ELEMENT to EMPTY >\
+]>';
+			var output = dtd.tokenize(input);
+			output = output.map(toString);
+			expect(output[2]).to.equal('ELEMENT.CATEGORY(31:ANY)');
+			expect(output[4]).to.equal('ELEMENT.CATEGORY(48:EMPTY)');
+		})
+		it('tokenizes ELEMENT.CONTENT', function(){
+			var input = '<!DOCTYPE note [\
+<!ELEMENT note (to,from,heading,body)>\
+<!ELEMENT to (#PCDATA)>\
+]>';
+			var output = dtd.tokenize(input);
+			output = output.map(toString);
+			expect(output[2]).to.equal('ELEMENT.CONTENT(31:()');
+			expect(output[13]).to.equal('ELEMENT.CONTENT(67:()');
+		})
+	})
+	describe('"element.content" lexer', function(){
+		it('tokenizes ELEMENT.CONTENT.CHILD', function(){
+			var input = '<!DOCTYPE note [\
+<!ELEMENT note (to,from,heading,body)>\
+<!ELEMENT to (#PCDATA)>\
+]>';
+			var output = dtd.tokenize(input);
+			output = output.map(toString);
+			expect(output[3]).to.equal('ELEMENT.CONTENT.CHILD(32:to)');
+			expect(output[14]).to.equal('ELEMENT.CONTENT.PCDATA(68:#PCDATA)');
+		})
+	})
+	describe('"element.content.delimiter" lexer', function(){
+		it('tokenizes ELEMENT.CONTENT.SEQUENCE', function(){
+			var input = '<!DOCTYPE note [\
+<!ELEMENT note (to,from,heading,body)>\
+]>';
+			var output = dtd.tokenize(input);
+			output = output.map(toString);
+			expect(output[4]).to.equal('ELEMENT.CONTENT.SEQUENCE(34:,)');
+			expect(output[6]).to.equal('ELEMENT.CONTENT.SEQUENCE(39:,)');
+			expect(output[8]).to.equal('ELEMENT.CONTENT.SEQUENCE(47:,)');
+		})
+		it('tokenizes ELEMENT.CONTENT.OR', function(){
+			var input = '<!DOCTYPE note [\
+<!ELEMENT note (to|from|heading|body)>\
+]>';
+			var output = dtd.tokenize(input);
+			output = output.map(toString);
+			expect(output[4]).to.equal('ELEMENT.CONTENT.OR(34:|)');
+			expect(output[6]).to.equal('ELEMENT.CONTENT.OR(39:|)');
+			expect(output[8]).to.equal('ELEMENT.CONTENT.OR(47:|)');
+		})
+	})
+	describe('"element.content.suffix" lexer', function(){
+		it('tokenizes ELEMENT.CONTENT.MIN-ONE-OCCURENCE', function(){
+			var input = '<!DOCTYPE note [\
+<!ELEMENT note (to+)>\
+<!ELEMENT note (to|from+)>\
+]>';
+			var output = dtd.tokenize(input);
+			output = output.map(toString);
+			expect(output[4]).to.equal('ELEMENT.CONTENT.MIN-ONE-OCCURENCE(34:+)');
+			expect(output[12]).to.equal('ELEMENT.CONTENT.MIN-ONE-OCCURENCE(60:+)');
+		})
+		it('tokenizes ELEMENT.CONTENT.ZERO-OR-MORE-OCCURENCE', function(){
+			var input = '<!DOCTYPE note [\
+<!ELEMENT note (to*)>\
+<!ELEMENT note (to|from*)>\
+]>';
+			var output = dtd.tokenize(input);
+			output = output.map(toString);
+			expect(output[4]).to.equal('ELEMENT.CONTENT.ZERO-OR-MORE-OCCURENCE(34:*)');
+			expect(output[12]).to.equal('ELEMENT.CONTENT.ZERO-OR-MORE-OCCURENCE(60:*)');
+		})
+		it('tokenizes ELEMENT.CONTENT.ZERO-OR-ONE-OCCURENCE', function(){
+			var input = '<!DOCTYPE note [\n' +
+				'<!ELEMENT note (to?)>\n' +
+				'<!ELEMENT note (to|from?)>\n' +
+				']>';
+			var output = dtd.tokenize(input);
+			output = output.map(toString);
+			expect(output[4]).to.equal('ELEMENT.CONTENT.ZERO-OR-ONE-OCCURENCE(35:?)');
+			expect(output[12]).to.equal('ELEMENT.CONTENT.ZERO-OR-ONE-OCCURENCE(62:?)');
+		})
+	})
+	describe('"element*" lexers', function(){
+		it('should tokenize mixed content 1', function(){
+			var input = '<!DOCTYPE note [\n' +
+				'<!ELEMENT note (#PCDATA|to|from|header|message)*>\n' +
+				']>';
+			var output = dtd.tokenize(input);
+			output = output.map(toString);
+			expect(output[ 1]).to.equal('ELEMENT(17:note)');
+			expect(output[ 2]).to.equal('ELEMENT.CONTENT(32:()');
+			expect(output[ 3]).to.equal('ELEMENT.CONTENT.PCDATA(33:#PCDATA)');
+			expect(output[ 4]).to.equal('ELEMENT.CONTENT.OR(40:|)');
+			expect(output[ 5]).to.equal('ELEMENT.CONTENT.CHILD(41:to)');
+			expect(output[ 6]).to.equal('ELEMENT.CONTENT.OR(43:|)');
+			expect(output[ 7]).to.equal('ELEMENT.CONTENT.CHILD(44:from)');
+			expect(output[ 8]).to.equal('ELEMENT.CONTENT.OR(48:|)');
+			expect(output[ 9]).to.equal('ELEMENT.CONTENT.CHILD(49:header)');
+			expect(output[10]).to.equal('ELEMENT.CONTENT.OR(55:|)');
+			expect(output[11]).to.equal('ELEMENT.CONTENT.CHILD(56:message)');
+			expect(output[12]).to.equal('ELEMENT.CONTENT.CLOSE(63:))');
+			expect(output[13]).to.equal('ELEMENT.CONTENT.ZERO-OR-MORE-OCCURENCE(64:*)');
+			expect(output[14]).to.equal('ELEMENT.END(65:>)');
+		})
+		it('should tokenize mixed content 2', function(){
+			var input = '<!DOCTYPE note [\n' +
+				'<!ELEMENT note (to,from,header,(message|body))>\n' +
+				']>';
+			var output = dtd.tokenize(input);
+			output = output.map(toString);
+			expect(output[ 1]).to.equal('ELEMENT(17:note)');
+			expect(output[ 2]).to.equal('ELEMENT.CONTENT(32:()');
+			expect(output[ 3]).to.equal('ELEMENT.CONTENT.CHILD(33:to)');
+			expect(output[ 4]).to.equal('ELEMENT.CONTENT.SEQUENCE(35:,)');
+			expect(output[ 5]).to.equal('ELEMENT.CONTENT.CHILD(36:from)');
+			expect(output[ 6]).to.equal('ELEMENT.CONTENT.SEQUENCE(40:,)');
+			expect(output[ 7]).to.equal('ELEMENT.CONTENT.CHILD(41:header)');
+			expect(output[ 8]).to.equal('ELEMENT.CONTENT.SEQUENCE(47:,)');
+			expect(output[ 9]).to.equal('ELEMENT.CONTENT(48:()');
+			expect(output[10]).to.equal('ELEMENT.CONTENT.CHILD(49:message)');
+			expect(output[11]).to.equal('ELEMENT.CONTENT.OR(56:|)');
+			expect(output[12]).to.equal('ELEMENT.CONTENT.CHILD(57:body)');
+			expect(output[13]).to.equal('ELEMENT.CONTENT.CLOSE(61:))');
+			expect(output[14]).to.equal('ELEMENT.CONTENT.CLOSE(62:))');
+			expect(output[15]).to.equal('ELEMENT.END(63:>)');
+		})
 	})
 })
